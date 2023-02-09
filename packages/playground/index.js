748316cabe961cbc239d9fbbfabc43d793e8f5ab
Frank <chengqifw@gmail.com>
2023-02-09 22:15:36 +0800

    update

---
+++ packages/playground/index.js
@@ -0,0 +1,45 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const server = (0, fastify_1.default)({});
+const opts = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    pong: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    }
+};
+server.get('/ping', opts, (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    return { pong: 'it worked!' };
+}));
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield server.listen({ port: 3000 });
+        const address = server.server.address();
+        const port = typeof address === 'string' ? address : address === null || address === void 0 ? void 0 : address.port;
+    }
+    catch (err) {
+        server.log.error(err);
+        process.exit(1);
+    }
+});
+start();
--- packages/playground/rollup.config.mjs
+++ packages/playground/rollup.config.mjs
@@ -1,18 +1,20 @@
-import resolve from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 
 export default {
-	input: 'dist/app.js',
+	input: 'index.js',
 	output: {
-		dir: 'dist/bundle.js',
+		dir: 'dist',
 		format: 'cjs'
 	},
 	plugins: [
-		resolve(),
-		commonjs(),
+		nodeResolve(),
+		commonjs({
+			include: ['node_modules/**']
+		}),
 		json(),
-		terser()
+		// terser()
 	]
 };
